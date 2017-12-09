60dc05977fc3251655e8030e813aa55b92a0aaf7
Unknown <asmonix@gmail.com>
2017-12-09 22:24:37 +0100

    dupa dupa 123

---
+++ Assets/Animations/dragoat/DragoatMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragoatMove : MonoBehaviour {
+
+    public Transform StartPositionDragoat;
+    public Transform EndPositionDragoat;
+    float StartTime;
+    float TotalDistanceToDestination;
+    public CthulhuMovement cthulu;
+
+	// Use this for initialization
+	protected void Start () {
+        StartTime = Time.time;
+        TotalDistanceToDestination = Vector3.Distance(StartPositionDragoat.position, EndPositionDragoat.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float currentDuration = Time.time - StartTime;
+        float jorneyFraction = currentDuration / TotalDistanceToDestination;
+        transform.position = Vector3.Lerp(StartPositionDragoat.position, EndPositionDragoat.position, jorneyFraction * 3);
+        if(Input.GetKeyDown("k"))
+        {
+            EndPositionDragoat.Translate(2f, 0, 0);//(Vector3.right)
+            StartPositionDragoat = transform;
+            StartTime = Time.time;
+            TotalDistanceToDestination = Vector3.Distance(StartPositionDragoat.position, EndPositionDragoat.position);
+            cthulu.SetVariables();
+        }
+        if (Input.GetKeyDown("j"))
+        {
+            EndPositionDragoat.Translate(-0.3f,0,0); //(Vector3.left)
+            StartPositionDragoat = transform;
+            StartTime = Time.time;
+            TotalDistanceToDestination = Vector3.Distance(StartPositionDragoat.position, EndPositionDragoat.position);
+            cthulu.SetVariables();
+        }
+
+
+
+
+    }
+}
---
+++ Assets/CthulhuMovement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CthulhuMovement : MonoBehaviour
+{
+
+    public Transform StartPositionCthulhu;
+    public Transform EndPositionCthulhu;
+    float StartTime;
+    float TotalDistanceToDestination;
+
+
+    // Use this for initialization
+    protected void Start()
+    {
+        StartTime = Time.time;
+        TotalDistanceToDestination = Vector3.Distance(StartPositionCthulhu.position, EndPositionCthulhu.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currentDuration = Time.time - StartTime;
+        float jorneyFraction = currentDuration / TotalDistanceToDestination;
+        transform.position = Vector3.Lerp(StartPositionCthulhu.position, EndPositionCthulhu.position, jorneyFraction * 3);
+
+    }
+
+    public void SetVariables()
+    {
+        StartPositionCthulhu = transform;
+        StartTime = Time.time;
+        TotalDistanceToDestination = Vector3.Distance(StartPositionCthulhu.position, EndPositionCthulhu.position);
+
+    }
+}
