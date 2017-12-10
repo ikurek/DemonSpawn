6e9ec0c0d8c361136555cf0a625c500bef69a3d4
Unknown <asmonix@gmail.com>
2017-12-10 12:50:06 +0100

    Muzyczka

---
+++ Assets/ScreenShake.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenShake : MonoBehaviour {
+
+    public Camera mainCam;
+
+    float shakeAmount = 0;
+
+        public void Awake()
+    {
+        if (mainCam == null)
+            mainCam = Camera.main;
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("y"))
+        {
+            Shake(1f, 0.2f);
+        }
+	}
+
+    public void Shake(float amt, float lengh)
+    {
+        shakeAmount = amt;
+        InvokeRepeating("BeginShake", 0, 0.01f);
+        Invoke("StopShake", lengh);
+    }
+
+    void BeginShake()
+    {
+        if (shakeAmount >0)
+        {
+            Vector3 camPos = mainCam.transform.position;
+
+            float offsetX = Random.value * shakeAmount * 2 - shakeAmount;
+            float offsetY = Random.value * shakeAmount * 2 - shakeAmount;
+            camPos.x += offsetX;
+            camPos.y += offsetY;
+
+            mainCam.transform.position = camPos;
+        }
+    }
+
+    void StopShake()
+    {
+        CancelInvoke("BeginShake");
+        mainCam.transform.localPosition = Vector3.zero;
+    }
+}
